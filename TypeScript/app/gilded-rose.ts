7518107eb3cdc0b5be92ba309dcfa2bab5f5cea2
Cake <cakemancerr@gmail.com>
2023-09-06 21:53:34 +0200

    convert for loop to forEach

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,32 +1,69 @@
-import { AgedBrie, Backseat, Conjured, Item, Regular, Sulfuras } from "@/items";
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
 
 export class GildedRose {
-  items: Item[]
+  items: Array<Item>;
 
-  constructor(items = [] as Item[]) {
+  constructor(items = [] as Array<Item>) {
     this.items = items;
   }
 
   updateQuality() {
-    this.items = this.items.map(item => {
-      switch (item.name) {
-        case 'Sulfuras, Hand of Ragnaros':
-          return new Sulfuras(item).update()
-        case 'Aged Brie':
-          return new AgedBrie(item).update()
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          return new Backseat(item).update()
-        case 'Conjured Mana Cake':
-          return new Conjured(item).update()
-        default:
-          return new Regular(item).update()
+    this.items.forEach(item => {
+      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.quality > 0) {
+          if (item.name != 'Sulfuras, Hand of Ragnaros') {
+            item.quality = item.quality - 1
+          }
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.sellIn < 11) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1
+              }
+            }
+            if (item.sellIn < 6) {
+              if (item.quality < 50) {
+                item.quality = item.quality + 1
+              }
+            }
+          }
+        }
+      }
+      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        item.sellIn = item.sellIn - 1;
+      }
+      if (item.sellIn < 0) {
+        if (item.name != 'Aged Brie') {
+          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (item.quality > 0) {
+              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+                item.quality = item.quality - 1
+              }
+            }
+          } else {
+            item.quality = item.quality - item.quality
+          }
+        } else {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1
+          }
+        }
       }
     });
 
-    return this.items
+    return this.items;
   }
-
 }
-
-
-
