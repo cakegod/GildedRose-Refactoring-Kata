d11c83ed44f6a6fce12168f453c0e25392cf90bd
Cake <cakemancerr@gmail.com>
2023-09-06 22:07:27 +0200

    flip to positive condition
    (cherry picked from commit 7dcdcccd859c5c0106ca9310030cd3869724257a)

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -38,14 +38,14 @@ export class GildedRose {
       item.sellIn = item.sellIn - 1;
 
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            this.decreaseQuality(item);
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
+        if (item.name === 'Aged Brie') {
           this.increaseQuality(item);
+        } else {
+          if (item.name === 'Backstage passes to a TAFKAL80ETC concert') {
+            item.quality = item.quality - item.quality
+          } else {
+            this.decreaseQuality(item);
+          }
         }
       }
     });
